16ca37d0131c892af4fd77ce06481d36b171a0e1
yunyoung <yunyoung.ju@gmail.com>
2019-01-29 12:53:38 +0900

    add image

--- calenderofprayAdventszeit/Secoundday.swift
+++ calenderofprayAdventszeit/Secoundday.swift
@@ -11,18 +11,24 @@ import Foundation
 import UIKit
 
 class Secoundday: UIViewController{
+    let bibleDic = [1:"1.png",2:"2.png",3:"3.png",4:"4.png",5:"5.png",6:"6.png",7:"7.png"]
+    var clickcount:Int = 1
     override func viewDidLoad() {
         super.viewDidLoad()
   
     }
     @IBAction func addText(_ button: UIButton!) {
-        
-        let imageview = TextInageview (frame: CGRect(x:100, y:300, width:290, height:30))
-        imageview.isUserInteractionEnabled = true
-        imageview.image = UIImage(named: "text1.png")
-        imageview.backgroundColor = UIColor.red
-        imageview.contentMode = .scaleAspectFit
-        self.view.addSubview(imageview)
+        if(clickcount < 8){
+            let yrect = 300+clickcount*10
+            let imageview = TextInageview (frame: CGRect(x:100, y:yrect, width:290, height:30))
+            imageview.isUserInteractionEnabled = true
+            imageview.image = UIImage(named: bibleDic[clickcount]!)
+            
+            imageview.contentMode = .scaleAspectFit
+            self.view.addSubview(imageview)
+            clickcount += 1
+        }
+
     }
 }
 
