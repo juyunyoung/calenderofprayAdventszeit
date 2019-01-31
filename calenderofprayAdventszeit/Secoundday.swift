5d36d7da715cb8a89ca9da561c967fa50edde0e1
yunyoung <yunyoung.ju@gmail.com>
2019-01-31 22:31:43 +0900

    play sound

--- calenderofprayAdventszeit/Secoundday.swift
+++ calenderofprayAdventszeit/Secoundday.swift
@@ -7,22 +7,41 @@
 //
 
 import Foundation
-
+import AVFoundation
 import UIKit
 
 class Secoundday: UIViewController{
-    let bibleDic = [1:"1.png",2:"2.png",3:"3.png",4:"4.png",5:"5.png",6:"6.png",7:"7.png"]
+ //   let bibleDic = [1:"1.png",2:"2.png",3:"3.png",4:"4.png",5:"5.png",6:"6.png",7:"7.png"]
     var clickcount:Int = 1
+    var bombSoundEffect: AVAudioPlayer? = AVAudioPlayer()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-  
+        
+        let path = Bundle.main.path(forResource: "44", ofType:"mp3")!
+        let url = URL(fileURLWithPath: path)
+        do {
+            
+            bombSoundEffect = try AVAudioPlayer(contentsOf: url)
+        } catch {
+            print("err sound file")
+        }
+        
     }
+    
     @IBAction func addText(_ button: UIButton!) {
+bombSoundEffect?.play()
+        
+
         if(clickcount < 8){
             let yrect = 300+clickcount*10
-            let imageview = TextInageview (frame: CGRect(x:100, y:yrect, width:290, height:30))
+            let xrect = 50+clickcount*10
+
+            let imageview = TextInageview (frame: CGRect(x:xrect, y:yrect, width:331, height:38))
             imageview.isUserInteractionEnabled = true
-            imageview.image = UIImage(named: bibleDic[clickcount]!)
+            imageview.contentMode = UIView.ContentMode(rawValue: 7)!
+            imageview.image = UIImage(named: String(clickcount)+".png")
+            imageview.restorationIdentifier = String(clickcount)
             
             imageview.contentMode = .scaleAspectFit
             self.view.addSubview(imageview)
