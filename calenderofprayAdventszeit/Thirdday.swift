dfc097213951e7c8bc993ce191af4a8fa8f39949
yunyoung <yunyoung.ju@gmail.com>
2019-02-01 12:55:20 +0900

    third

---
+++ calenderofprayAdventszeit/Thirdday.swift
@@ -0,0 +1,30 @@
+//
+//  Thirdday.swift
+//  calenderofprayAdventszeit
+//
+//  Created by 주 윤령 on 01/02/2019.
+//  Copyright © 2019 주 윤령. All rights reserved.
+//
+
+import UIKit
+
+class Thirdday: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
